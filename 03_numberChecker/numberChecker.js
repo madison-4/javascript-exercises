79d55833eba98002efdbc576cabd0cce6ba0b865
noxcodes <samingosi97@gmail.com>
2025-11-27 11:09:24 +0300

    finish excercise 3

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,5 +1,8 @@
+#!/usr/bin/node
+// function to test if a number is greater than 10
+// return true if it is greater than or equal to 10
 function numberChecker(number) {
-  if (number === 6) {
+    if (Number(number) >= 10) {
     return true;
   } else {
     return false;
